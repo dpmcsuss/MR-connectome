c55fc5fca7c5ad3ada79551cee9b1f9bf6951e26
Randal Burns <randal@cs.jhu.edu>
2012-04-10 10:21:28 -0400

    First cython version working.

--- python/setup.py
+++ python/setup.py
@@ -6,14 +6,14 @@ import numpy
 
 ext_modules = [Extension("fiber",["fiber.pyx"],include_dirs=[numpy.get_include()]),
                Extension("roi",["roi.pyx"],include_dirs=[numpy.get_include()]),
-               Extension("roi",["roi.pyx"],include_dirs=[numpy.get_include()]),
+               Extension("fibergraph",["fibergraph.pyx"],include_dirs=[numpy.get_include()]),
 ]
 # You can add directives for each extension too
 # by attaching the `pyrex_directives`
 for e in ext_modules:
     e.pyrex_directives = {"boundscheck": False}
 setup(
-    name = "cython accelerator for processing voxels lists",
+    name = "cython accelerator for processing MRCAP graphs",
     cmdclass = {"build_ext": build_ext},
     ext_modules = ext_modules
 )
